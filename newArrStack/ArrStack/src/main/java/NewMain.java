ae1f26567a4988f088742afa68428aa451660b2c
LundiLanga <alulundilanga@gmail.com>
2021-10-05 10:07:47 +0200

    done Signed-off-by:LundiLanga <alulundilanga@gmail.com>

---
+++ newArrStack/ArrStack/src/main/java/NewMain.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Aluncedo Lundi Langa
+ */
+import java.util.*;
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       ArrStack as = new ArrStack(3);
+       as.set(0, "Aluncedo");
+       as.set(1, "Lundi");
+       as.set(2, "Langa");
+       System.out.println(as.get(1));
+       ArrStack ss = new ArrStack(1);
+       ss.set(0, "Sakhumzi Chuthsela");
+       System.out.println(ss.get(0));
+       System.out.println(as.size());
+       as.resize();
+       System.out.println(as.size());
+       as.add(1, "Endinako");
+       System.out.println(as.get(1));
+       as.remove(0);
+       System.out.println(as.size());
+    }
+    
+}
